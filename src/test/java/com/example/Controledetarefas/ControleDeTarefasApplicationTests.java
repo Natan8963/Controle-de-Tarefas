003c149d3857cfa1752a1fad9aac7bcf0b1ad6cc
Nathan8963 <nathan8963@gmail.com>
2024-01-12 13:30:44 -0300

    primeiro commit

---
+++ src/test/java/com/example/Controledetarefas/ControleDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Controledetarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
